1aaddd4c9a00e9f04bceb7f163a8047672f605bd
Kamila Lemaire <kb.lemaire@gmail.com>
2023-04-13 18:21:55 -0600

    adding test cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,32 +1,34 @@
 const request = require('request');
 
 const args = process.argv.slice(2);
-let breed = args[0];
-const URL = "https://api.rhecatapi.com/v1/breeds/search?q=" + breed;
+let breedName = args[0];
 
 
-const getCatInfo = () => {
+const fetchBreedDescription = function(breedName, callback) {
+  const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
   request(URL, (error, response, body) => {
+
     if (error) {
-      console.log(`Request failed. Please see error details ${error}`);
+      callback(error, null);
       return;
     }
-
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body);
-
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body);
+console.log("URL:", URL);
     const data = JSON.parse(body);
-    console.log("DATA:", data);
     if (data.length === 0) {
-      return console.log("Breed could not be found");
+      callback("Breed could not be found", null);
+      return;
     }
-    console.log("FIRST ENTRY:", data[0].description);
-
+    callback(null, data[0].description);
 
   });
 };
 
-getCatInfo();
+
+module.exports = {
+  fetchBreedDescription
+};
 
 
 
